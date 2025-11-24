fc0f24c17a2ce7006c1a4650baf05b500533b42e
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-24 11:33:24 +0300

    add classwork changes

--- main.cpp
+++ main.cpp
@@ -1,48 +1,95 @@
 #include <iostream>
 
 struct IntArray {
-    int* a;
-    size_t k;
-    void add(int i);
-    int get(size_t id) const noexcept;
-    size_t size() const noexcept;
-    int last() const noexcept;
-    IntArray(int i);
-    ~IntArray();
+  void add(int i);
+  int get(size_t id) const noexcept;
+  size_t getSize() const noexcept;
+  int last() const noexcept;
+  IntArray(int i);
+  ~IntArray();
+  int* a;
+  size_t size;
+  int at(size_t id()) const;
+  IntArray(const IntArray & rhs); //конструктор копирования
+  IntArray & operator=(const IntArray & rhs); //оператор копирующего присваивания 
+  IntArray (IntArray && rhs);
+  IntArray & operator=(IntArray && rhs);
 };
 
 IntArray::~IntArray() {
-    delete[] a;
+  delete[] a;
 }
 
 IntArray::IntArray(int i) :
-    a(new int[1]),
-    k(1)
+  a(new int[1]),
+  size(1)
 
 {
-    *a = i;
+  *a = i;
 }
 
 int IntArray::get(size_t id) const noexcept {
-    return a[id];
+  return a[id];
 }
 
-size_t IntArray::size() const noexcept {
-    return k;
+size_t IntArray::getSize() const noexcept {
+  return size;
 }
 
 int IntArray::last() const noexcept {
-    return get(size() - 1);
+  return get(getSize() - 1);
 }
 
 void IntArray::add(int i) {
-    int* tmp = new int[size() + 1];
-    for (size_t i = 0; i < size(); ++i) {
-        tmp[i] = get(i);
-    }
-    delete[] a;
-    a = tmp;
-    ++k;
+  int* tmp = new int[getSize() + 1];
+  for (size_t i = 0; i < getSize(); ++i) {
+    tmp[i] = get(i);
+  }
+  delete[] a;
+  a = tmp;
+  ++size;
+}
+
+IntArray::IntArray (const IntArray & rhs) :
+  a(new int [rhs.getSize()]),
+  size(rhs.getSize())
+{
+  for (size_t i = 0; i < getSize(); ++i)
+  {
+    a[i] = rhs.get(i);
+  }
+}
+
+IntArray & IntArray::operator=(const IntArray & rhs)
+{
+  int * tmp = new int [rhs.getSize()];
+  for (size_t i = 0; i < rhs.getSize(); ++i)
+  {
+    tmp[i] = rhs.get(i);
+  }
+  delete[] a;
+  a = tmp;
+  size = rhs.getSize();
+  return *this; //то, что слева
+}
+
+IntArray::IntArray (IntArray && rhs):
+a(rhs.a),
+size(rhs.getSize())
+{
+  rhs.a = nullptr;
+}
+
+//IntArray c (std::move(b)) перемещение
+//IntArray p (a) копирование
+
+IntArray & IntArray::operator=(IntArray && rhs)
+{
+  delete[] a;
+  a = rhs.a;
+  size = rhs.size;
+  rhs.a = nullptr;
+  return * this;
 }
 
 int main()
@@ -50,26 +97,24 @@ int main()
   int next = 0;
   std::cin >> next;
 
-    try {
-        IntArray a(next);    //вызов конструктора по умолчанию
-        //a.add(next);   //add
-        while (std::cin >> next) {
-            a.add(next);
-        }
-        if (std::cin.fail() && !(std::cin.eof())) {
-            return 1;
-        }
-        size_t count = 1;
+  try {
+    IntArray a(next);    //вызов конструктора по умолчанию
+    while (std::cin >> next) {
+      a.add(next);
+    }
+    if (std::cin.fail() && !(std::cin.eof())) {
+      return 1;
+    }
+    size_t count = 1;
 
-        for (size_t i = 0; i < a.size() - 1; ++i) {
-            int d = a.get(i);
-            // std::cout << d;
-            count += !(d % a.last());
-        }
-        std::cout << count << "\n";
-        return 0;
+    for (size_t i = 0; i < a.getSize() - 1; ++i) {
+      int d = a.get(i);
+      count += !(d % a.last());
     }
-    catch (const std::bad_alloc()) {
-        return 2;
-    }
+    std::cout << count << "\n";
+  }
+  catch (const std::bad_alloc()) {
+    return 2;
+  }
+  return 0;
 }
